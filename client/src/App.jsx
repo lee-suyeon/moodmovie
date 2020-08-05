c728cc71f5153679d3db05dd6ea842ebb73b8f92
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-08-05 20:06:13 +0900

    client setting

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/login' component={LoginPage} />
+        <Route exact path='/register' component={RegisterPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
