5f814714003d986d0441e44e7b6227a5868a8bd5
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-08-07 15:53:28 +0900

    Add MainImage Component

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,2 +1,5 @@
 // export const USER_SERVER = '/api/users';
 
+export const API_KEY = "64ae4a6cf68f6cb02e4f13eff4fc8096";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/";
--- client/src/components/views/RecommendPage/RecommendPage.jsx
+++ client/src/components/views/RecommendPage/RecommendPage.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import MainImage from './Section/MainImage';
+import { API_KEY, API_URL, IMAGE_URL } from '../../../Config';
 
 function RecommendPage() {
+  const [movies, setMovies] = useState([]);
+  const [mainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}discover/movie?api_key=${API_KEY}&language=ko&sort_by=popularity.desc&with_genres=28&page=1`;
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response.results.slice(0, 4)); // 4개만 가져오기
+        setMovies([ ...response.results ]);
+        setMainMovieImage(response.results[1]);
+      })
+  }, [])
+
   return (
-    <div>
-      추천페이지
+    <div style={{ width: "100%", margin: "0"}}>
+      {/* main Image */}
+      {mainMovieImage && 
+        <MainImage 
+          title={mainMovieImage.title}
+          originalTitle={mainMovieImage.original_title}
+          overview={mainMovieImage.overview}
+          image={`${IMAGE_URL}w1280${mainMovieImage.backdrop_path}`} />
+        
+        }
+
+      <div style={{ width: "85%", margin: "1rem auto"}}>
+        <h2>추천 영화</h2>
+        <hr />
+        {/* Movie Grid Cards */}
+      </div>
     </div>
   )
 }
+
+
 
 export default RecommendPage
